ef0eba8946cf5a422e9cc7205c84ed015a066cb2
sourabhpanadare <sourabhpanadare@foxymoron.org>
2017-03-15 16:02:14 +0530

    Modification 1

--- js/common.js
+++ js/common.js
@@ -4,8 +4,30 @@ $(document).ready(function(){
   var w = $(window).width();
   var h = $(window).height();
 
-  
-  
+
+  $('.owl-carousel').owlCarousel({
+    margin: 10,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 3
+      },
+      768: {
+        items: 4
+      },
+      992: {
+        items: 5
+      }
+    },
+    nav: true,
+    loop: true,
+    dots: false,
+    navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+    touchDrag  : true,
+    mouseDrag  : false
+  });
   
 });
 
