214f2955d0bfed112b75332187477387d568f5e2
sourabhpc <sourabhpanadare@yahoo.co.in>
2017-03-22 07:23:03 +0530

    CSS JS Change

--- js/common.js
+++ js/common.js
@@ -5,34 +5,6 @@ $(document).ready(function(){
   var h = $(window).height();
 
 
-  /*----------------------------OWL -Carousel--------------------------------*/
-
-  $('.owl-carousel').owlCarousel({
-
-    margin: 10,
-    responsive: {
-      0: {
-        items: 1
-      },
-      600: {
-        items: 3
-      },
-      768: {
-        items: 4
-      },
-      992: {
-        items: 5
-      }
-    },
-    nav: true,
-    loop: true,
-    dots: false,
-    navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
-    touchDrag  : true,
-    mouseDrag  : false
-
-  });
-  
 });
 
 /*----------------------------------------------------Window Resize Function---------------------------------------------*/
@@ -41,11 +13,13 @@ $(window).resize(function(){
   var w = $(window).width();
   var h = $(window).height();
 
-
-
 });
 
+/*----------------------------------------------------Window Rotate Function------------------------------------------*/
 
+$( window ).on( "orientationchange", function( event ) {
+
+});
 
 /*----------------------------------------------------Window Load Function---------------------------------------------*/
 
@@ -53,15 +27,11 @@ $(window).load(function() {
 
 });
 
-
-
 /*----------------------------------------------------Window Scroll Function---------------------------------------------*/
 
 $(window).scroll(function(){
 
 });
-
-
 
 /*-------------------------------------Common JS--------------------------------------*/
 var w = $(window).width();
